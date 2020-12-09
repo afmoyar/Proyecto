05c9ae93d885c57a3da0346d13259f408d255cf6
afmoyar <afmoyar@unal.edu.co>
2020-12-09 10:29:49 -0500

    updating overloaded method

--- key_generator.h
+++ key_generator.h
@@ -105,6 +105,25 @@ int getIndex(std::vector<std::string> v, std::string K)
         return -1;
     }
 }
+int getIndex(std::vector<int> v, int K)
+{
+    auto it = find(v.begin(), v.end(), K);
+ 
+    // If element was found
+    if (it != v.end()) 
+    {
+     
+        // calculating the index
+        // of K
+        int index = it - v.begin();
+        return index;
+    }
+    else {
+        // If the element is not
+        // present in the vector
+        return -1;
+    }
+}
 
 std::string encodeKeyIds(std::vector<std::string> pool, std::vector<std::string> nodeKeys){
   std::string codedKeyIds;
